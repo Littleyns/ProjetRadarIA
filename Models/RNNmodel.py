f10c10edb937c4bf9932ab2265d52ffd95b0e74b
zarroukmh <zarroukm@3il.fr>
2023-10-23 01:01:08 +0200

    rnn

--- Models/RNNmodel.py
+++ Models/RNNmodel.py
@@ -13,42 +13,29 @@ from sklearn.preprocessing import StandardScaler
 from Models.BasicAutoEncoder import BasicAutoEncoder
 
 
-class BasicCNNModel:
+class RNNModel:
     def __init__(self, model = None):
         self.model = model
     class Trainer:
         def __init__(self, input_shape, output_dim):
             # Créez un modèle séquentiel
-            self.input_shape = input_shape
-            model = keras.Sequential()
+            self.model = Sequential()
 
-            # Couche de convolution 1D avec 32 filtres, une fenêtre de 3 et une fonction d'activation ReLU
-            model.add(layers.Conv1D(32, kernel_size=3, activation='relu', input_shape=(input_shape[1], 1)))
-
-            # Couche de convolution 1D supplémentaire
-            model.add(layers.Conv1D(64, kernel_size=3, activation='relu'))
-
-            # Aplatissement des données pour la couche dense
-            model.add(layers.Flatten())
-
-            # Couche dense (entièrement connectée) avec 128 neurones
-            model.add(layers.Dense(128, activation='relu'))
-            model.add(layers.Dense(64, activation='relu'))
-            # Couche de sortie avec une seule unité pour la classification binaire (par exemple, sigmoid pour la classification binaire)
-            model.add(layers.Dense(output_dim, activation='sigmoid'))
+            self.model.add(SimpleRNN(units=64, input_shape=(sequence_length, 1))
+            self.model.add(Dense(units=1, activation='linear'))
 
             # Compiler le modèle avec une fonction de perte (loss) appropriée et un optimiseur
-            model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+             self.model.compile(loss='mean_squared_error', optimizer='adam')
 
             # Résumé du modèle
-            model.summary()
+            self.model.summary()
             self.model = model;
 
 
 
 
 
-        def train(self, X_train, y_train, epochs = 30, batch_size = 1, validation_data = None):
+        def train(self, X_train, y_train, epochs = 30, batch_size = 10, validation_data = None):
             # Entraînez le modèle avec vos données d'entraînement et vos étiquettes
             self.X_train = X_train
             history = self.model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_data=validation_data)
@@ -81,9 +68,9 @@ if __name__ == "__main__":
     data, labels = data_loader.load_data()
     radar_dataset = RadarDataSet(data, labels, 0.05)
 
-    trainer = BasicCNNModel.Trainer(radar_dataset.X_train.shape, 180)
+    trainer = RNNModel.Trainer(radar_dataset.X_train.shape, 180)
     history = trainer.train(radar_dataset.X_train, radar_dataset.y_train, epochs = 30, batch_size=10, validation_data=(radar_dataset.X_test,radar_dataset.y_test))
     learningCurvePloter = LearningCurvesPlot()
     learningCurvePloter.evaluate(history)
-    trainer.saveModel("basicCNNModel4_moreData")
+    trainer.saveModel("RNNModel4_moreData")
 
